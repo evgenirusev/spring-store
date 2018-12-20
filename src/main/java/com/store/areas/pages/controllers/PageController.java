35419566110c5924444136b160f9b64b7255c700
rusev9 <evg.rusev@gmail.com>
2018-12-21 00:38:26 +0200

    PageController

---
+++ src/main/java/com/store/areas/pages/controllers/PageController.java
@@ -0,0 +1,30 @@
+package com.store.areas.pages.controllers;
+
+import com.store.abstractions.controller.BaseController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController extends BaseController {
+
+    @GetMapping("/")
+    public ModelAndView index() {
+        return super.view("views/index", "SpringAnswers");
+    }
+
+    @GetMapping("/contact")
+    public ModelAndView contactPage() {
+        return super.view("views/contact", "Contact");
+    }
+
+    @GetMapping("/about")
+    public ModelAndView aboutPage() {
+        return super.view("views/about", "About");
+    }
+
+    @GetMapping("/home")
+    public ModelAndView home() {
+        return super.view("views/index", "Home");
+    }
+}
