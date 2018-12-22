45be247aaf1bc16044920dfd5741dc76b6983943
rusev9 <evg.rusev@gmail.com>
2018-12-22 23:03:29 +0200

    Brands

--- src/main/java/com/store/areas/brand/models/view/BrandViewModel.java
+++ src/main/java/com/store/areas/brand/models/view/BrandViewModel.java
@@ -1,7 +1,13 @@
 package com.store.areas.brand.models.view;
 
+import com.store.areas.product.models.service.ProductServiceModel;
+
+import java.util.Set;
+
 public class BrandViewModel {
     private String name;
+
+    private Set<ProductServiceModel> products;
 
     public String getName() {
         return name;
@@ -9,5 +15,13 @@ public class BrandViewModel {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Set<ProductServiceModel> getProducts() {
+        return products;
+    }
+
+    public void setProducts(Set<ProductServiceModel> products) {
+        this.products = products;
     }
 }
