83445b49d1b9772a4d7cf7941968e7bc4c382e1d
rusev9 <evg.rusev@gmail.com>
2019-01-02 01:26:08 +0200

    Sale entity

---
+++ src/main/java/com/store/areas/sale/entities/Sale.java
@@ -0,0 +1,52 @@
+package com.store.areas.sale.entities;
+
+import com.store.areas.product.entities.Product;
+import com.store.areas.user.entities.User;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "sales")
+public class Sale {
+
+    private String id;
+
+    private Product product;
+
+    private User user;
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(
+            name = "UUID",
+            strategy = "org.hibernate.id.UUIDGenerator"
+    )
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @ManyToOne(cascade = CascadeType.REMOVE)
+    @JoinColumn(name = "product_id", referencedColumnName = "id")
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    @ManyToOne(cascade = CascadeType.REMOVE)
+    @JoinColumn(name = "user_id",referencedColumnName = "id")
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
